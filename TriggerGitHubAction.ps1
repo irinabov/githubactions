b60fe05fb3a79351cb5fd584b4d12ccaa6a6c5e2
Stefan Stranger <stefan@stranger.nl>
2020-01-29 19:19:08 +0100

    Added script for web request

---
+++ TriggerGitHubAction.ps1
@@ -0,0 +1,31 @@
+<#
+    Triggering Github Actions from a webhook call
+#>
+
+#region variables
+$GithubToken = $env:githubtoken # Personal Access Token stored as environment variable
+$GithubUserName = 'stefanstranger'
+$GithubRepo = 'githubactions'
+#endregion
+
+$uri = ('https://api.github.com/repos/{0}/{1}/dispatches' -f $GithubUserName, $GithubRepo)
+
+#region web request call
+$Body = @{
+    'event_type' = 'demo' #used in the if condition of the Github Action
+} | ConvertTo-Json
+
+
+$params = @{
+    ContentType = 'application/json'
+    Headers     = @{
+        'authorization' = "token $($GithubToken)"
+        'accept'        = 'application/vnd.github.everest-preview+json'
+    }
+    Method      = 'Post'
+    URI         = $Uri
+    Body        = $Body
+}
+  
+Invoke-RestMethod @params -verbose
+#endregion
